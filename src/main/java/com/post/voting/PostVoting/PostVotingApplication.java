4095e0f92e2db30020c4d7fbdfc387288ac89864
truong <vle@tibco.com>
2020-08-24 13:41:10 +0700

    init project

---
+++ src/main/java/com/post/voting/PostVoting/PostVotingApplication.java
@@ -0,0 +1,13 @@
+package com.post.voting.PostVoting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostVotingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostVotingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/post/voting/PostVoting/PostVotingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.post.voting.PostVoting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostVotingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
